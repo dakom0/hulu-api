67b002c0476c06f661288341fc6069690d4db157
DAMI KOMOLAFE <damik94@gmail.com>
2020-09-15 21:49:07 -0400

    first commit

---
+++ src/VideoCard.js
@@ -0,0 +1,32 @@
+import React, { forwardRef } from 'react'
+import './VideoCard.css'
+import TextTruncate from "react-text-truncate";
+import ThumbUpSharpIcon from "@material-ui/icons/ThumbUpSharp";
+
+const VideoCard = forwardRef(({ movie }, ref) => {
+    const base_url = "https://image.tmdb.org/t/p/original/";
+    
+    return (
+    <div ref={ref} className='videoCard'>
+      <img
+        src={`${base_url}${movie.backdrop_path || movie.poster_path}`}
+        alt=''
+      />
+
+      <TextTruncate
+        line={1}
+        element='p'
+        truncateText='...'
+        text={movie.overview}
+      />
+      <h2>{movie.title || movie.original_name}</h2>
+      <p className='movieStats'>
+        {movie.release_date || movie.first_air_date} |
+        <span style={{ paddingRight: "5px" }}>{movie.vote_count}</span>
+        <ThumbUpSharpIcon />
+      </p>
+    </div>
+  );
+});
+
+export default VideoCard;
---
+++ src/requests.js
@@ -0,0 +1,15 @@
+const API_KEY = '467f27c4a9a942424614c7bc68d55138';
+
+export default {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchAction: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedy: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorror: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomance: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchMystery: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+    fetchSciFi: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+    fetchWestern: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+    fetchAnimation: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+    fetchTV: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
+}
